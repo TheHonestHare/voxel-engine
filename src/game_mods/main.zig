4099f8fac39fda3df9d0a677a3d4ef1457565352
TheHonestHare <dracenlim@gmail.com>
2024-07-09 16:21:33 -0600

    Added some important exports

--- src/game_mods/main.zig
+++ src/game_mods/main.zig
@@ -1,15 +1,19 @@
 const std = @import("std");
-const zware = @import("zware");
 const fp = @import("mod_folder_parser.zig");
-const DAG = @import("dep_graph.zig").ModDAG;
+
+pub const DAG = @import("dep_graph.zig").ModDAG;
+pub const hashModFolder = fp.hashModFolder;
+pub const Mod = fp.Mod;
+pub const Data = fp.Data;
+pub const MetaData = fp.MetaData;
 // TODO: maybe use asychronous io for files etc instead of native std.fs
+// TODO: decide if next 2 functions should go in seperate files
 /// all returned values are allocated with ally, scratch is temporary memory that gets freed
-pub fn parseAllModsDir(ally: std.mem.Allocator, scratch: std.mem.Allocator, all_mods_dir: std.fs.Dir) !struct { []fp.Mod, DAG } {
+pub fn parseAllModsDir(ally: std.mem.Allocator, scratch: std.mem.Allocator, all_mods_dir: std.fs.Dir) !struct { []Mod, DAG } {
     const mod_slice = try getAllMods(ally, all_mods_dir);
-    errdefer for(mod_slice) |mod| mod.deinit(ally); 
+    errdefer for (mod_slice) |mod| mod.deinit(ally);
     const order_to_load = try DAG.create(ally, scratch, mod_slice);
     return .{ mod_slice, order_to_load };
-
 }
 /// dir must be opened with iterating capability
 pub fn getAllMods(ally: std.mem.Allocator, all_mods_dir: std.fs.Dir) ![]fp.Mod {
